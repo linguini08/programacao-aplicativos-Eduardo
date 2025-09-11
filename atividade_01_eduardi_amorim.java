9a625acb4effed3751421ad2c53c0b68dd248c9f
linguini08 <eduardoamori1506@gmail.com>
2025-09-11 20:29:41 -0300

    Add files via upload

---
+++ atividade_01_eduardi_amorim.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+
+        int A;
+        int B;
+        int C;
+        int D;
+
+        A = 25;
+        B = A * 3;
+        C = 10;
+        D = A/C;
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+
+}}
