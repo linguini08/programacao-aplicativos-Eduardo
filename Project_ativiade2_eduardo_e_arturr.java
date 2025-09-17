4807bd98bf2739bb85f226755e3980fc14e9f450
linguini08 <eduardoamori1506@gmail.com>
2025-09-16 21:18:39 -0300

    Add files via upload

---
+++ Project_ativiade2_eduardo_e_arturr.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_ativiade2_eduardo_e_arturr;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Project_ativiade2_eduardo_e_arturr {
+    
+   
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("ola mundo");
+        
+    }
+}
